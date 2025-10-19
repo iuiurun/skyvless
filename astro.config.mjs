26c28795de38fee9c97a93e29dc81f35393ecb80
iuiurun <73160994+iuiurun@users.noreply.github.com>
2025-10-20 03:17:34 +0800

    Revert "Update astro.config.mjs"
    This reverts commit 64e6da19a024b9526f4deb3948b195018e989878.

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://966911.xyz/",
+	site: "https://fuwari.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
